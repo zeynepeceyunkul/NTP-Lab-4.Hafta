75e40f2daeef8827c816fa38ad39f11358ced4ba
zeynepeceyunkul <eceyunkul44@gmail.com>
2024-11-05 22:38:55 +0300

    Adres Defteri

---
+++ AdresDefteri/AdresDefteri/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdresDefteri
+{
+    // Kisi sınıfı tanımı
+    class Kisi
+    {
+        // Kişi bilgilerini saklayacak alanlar
+        string Ad;
+        string Soyad;
+        string TelefonNo;
+
+        // Yapıcı metot, kişiyi oluştururken ad, soyad ve telefon numarası alır
+        public Kisi(string ad, string soyad, string telefonno)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            TelefonNo = telefonno;
+        }
+
+        // Kişi bilgilerini döndüren bir metot
+        public string KisiBilgisi()
+        {
+            return Ad + " " + Soyad + " " + TelefonNo;
+        }
+    }
+
+    // Programın başlangıç noktası
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kisi sınıfından bir örnek oluştur ve bilgilerini göster
+            Kisi kisi1 = new Kisi("Zeynep Ece", "YÜnkül", "555");
+            Console.WriteLine("Kişi Bilgileri: " + kisi1.KisiBilgisi());
+
+            // Kullanıcının bir tuşa basmasını bekle
+            Console.ReadKey();
+        }
+    }
+}
